469d655b64f2503a3f87e8083cf6d5e24623f9df
sekhar76405 <ssjio5402@gmail.com>
2023-04-16 16:37:06 +0530

    testing with new DB

--- db_connect.js
+++ db_connect.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: "postgres",
-    password: "root",
+    password: "lostpets",
     database: "lostpets",
-    host: "localhost",
+    host: "http://lostpets-db.cgtwrl4u4ygc.us-west-2.rds.amazonaws.com/",
     port: 5432
 });
 
